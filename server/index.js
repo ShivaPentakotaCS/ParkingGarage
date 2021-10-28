b189b0a08857d74863df853e1e680f812e1bfb78
Shiva Pentakota <shivapentakota@gmail.com>
2021-10-28 17:01:07 -0400

    server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    passowrd: "password",
+    database: "Enter DB Name"
+})
 
 app.get("/", (req,res) => {
-    res.send("workin")
+    const sqlInsert = "write sql query"
+    db.query(sqlInsert, (err,result)=> {
+        res.send("workin")
+    })
 })
 app.listen(3001, ()=> {
     console.log('runnin')
